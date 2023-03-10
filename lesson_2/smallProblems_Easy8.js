194903f897ea2fa6a5f548e61121833793c71007
Sergio Ravera <sergioravera@hotmail.de>
2023-03-10 17:02:39 +0100

    added smallProblem 8

---
+++ lesson_2/smallProblems_Easy8.js
@@ -0,0 +1,36 @@
+// function takes one number as argument
+// is number evenly divisible by 4? = true
+// unless is divisible by 100 = false
+// unless is divisible by 400 = true
+
+// given input greater than 0
+
+function isLeapYear(year) {
+  let divisibleBy4 = year % 4 === 0;
+  let divisibleBy100 = year % 100 === 0;
+  let divisibleBy400 = year % 400 === 0;
+
+  if (divisibleBy400 && divisibleBy100 && divisibleBy4) {
+    return true;
+  } else if (divisibleBy100 && divisibleBy4) {
+    return false;
+  } else if (divisibleBy4) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isLeapYear(2016));      // true
+console.log(isLeapYear(2015));      // false
+console.log(isLeapYear(2100));      // false
+console.log(isLeapYear(2400));      // true
+console.log(isLeapYear(240000));    // true
+console.log(isLeapYear(240001));    // false
+console.log(isLeapYear(2000));      // true
+console.log(isLeapYear(1900));      // false
+console.log(isLeapYear(1752));      // true
+console.log(isLeapYear(1700));      // false
+console.log(isLeapYear(1));         // false
+console.log(isLeapYear(100));       // false
+console.log(isLeapYear(400));       // true
