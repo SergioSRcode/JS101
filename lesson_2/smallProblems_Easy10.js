edac1d4cf825fab5cea4e7f6ca4309ba867d5dd7
Sergio Ravera <sergioravera@hotmail.de>
2023-03-11 20:17:03 +0100

    added smallProblems 10

--- lesson_2/smallProblems_Easy10.js
+++ lesson_2/smallProblems_Easy10.js
@@ -1,30 +1,12 @@
-// compute sum of all numbers between 1 and an integer passed as argument
-// the numbers inbetween must be multiples of 3 or 5
-
-// given integer greater than 1
-
-// start
-
-/*
-function (number)
-  ARRAY = all numbers between 1 and given number (inklusive)
-  loop (counter = 1; while counter < number; counter += 1)
-    ARRAY.push(counter);
-
-  SORTED_ARRAY = ARRAY.filter(num => num % 3 === 0 || num % 5 === 0)
-  Return SORTED_ARRAY.reduce(num => num + num);
-
-*/
-
 function multisum(number) {
-  let unsortedArray = [];
+  let UnsortedArray = [];
 
   for (let i = 1; i <= number; i++) {
-    unsortedArray.push(i);
+    UnsortedArray.push(i);
   }
-  // eslint-disable-next-line max-len
-  let sortedArray = unsortedArray.filter(num => (num % 3 === 0) || (num % 5 === 0));
-  return sortedArray.reduce((acc, num) => acc + num);
+
+  let SortedArray = UnsortedArray.filter(num => (num % 3 === 0) || (num % 5 === 0));
+  return SortedArray.reduce((acc, num) => acc + num);
 }
 
 console.log(multisum(3));       // 3
