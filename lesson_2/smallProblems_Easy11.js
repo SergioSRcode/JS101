fa886d2fec449a1addb773b1229c13f81306bd87
Sergio Ravera <sergioravera@hotmail.de>
2023-03-12 14:56:49 +0100

    added smallProblems11

---
+++ lesson_2/smallProblems_Easy11.js
@@ -0,0 +1,43 @@
+// Objective: return UTF-16 string value of string argument
+// retrieve the UTF-16 string value of every character in given string
+// get sum of retrieved values.
+
+// start
+/*
+FUNCTION (string)
+  ARRAY = [];
+
+  LOOP (counter 0; while counter < string.length; counter += 1)
+    ARRAY.push(string.charCodeAt(counter))
+
+  RESULT = ARRAY.reduce((acc, num) => acc + num)
+  RETURN RESULT.
+
+*/
+
+function utf16Value(string) {
+  let utf16OfChars = [];
+
+  if (string === "") {
+    return 0;
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    utf16OfChars.push(string.charCodeAt(i));
+  }
+  let sumOfChars = utf16OfChars.reduce((acc, num) => acc + num);
+  return sumOfChars;
+}
+
+console.log(utf16Value("Four score"));
+console.log(utf16Value("Launch School"));
+console.log(utf16Value("a"));
+console.log(utf16Value(""));
+
+// The next three lines demonstrate that the code
+// works with non-ASCII characters from the UTF-16
+// character set.
+const OMEGA = "\u03A9";             // UTF-16 character 'Ω' (omega)
+console.log(utf16Value(OMEGA));                  // 937
+console.log(utf16Value(OMEGA + OMEGA + OMEGA));  // 2811
+
