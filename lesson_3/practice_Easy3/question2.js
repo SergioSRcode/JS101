a5b761872e2cc12319133281a87844cf4275ca10
Sergio Ravera <sergioravera@hotmail.de>
2023-05-12 07:00:02 +0200

    added question2

---
+++ lesson_3/practice_Easy3/question2.js
@@ -0,0 +1,17 @@
+// What will the following code output?
+
+console.log([1, 2, 3] + [4, 5]);
+
+// Try to answer without running the code.
+
+// guess
+/*
+1) implicit type coercion: array/object => string
+2) concatenation of both strings due to the binary + operator
+3) print to the console: "1, 2, 3, 4, 5"
+*/
+
+//adjustment after running the code
+/*
+adj) console prints: "1, 2, 34, 5" as there is no comma after the num 3!
+*/
